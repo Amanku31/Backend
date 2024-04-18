e79c4f64bcc512c6d2a3c6ed15706e13f9b673c8
Amanku31 <amankumar74123@gmail.com>
2024-04-19 01:21:22 +0530

    Utils Code

--- src/index.js
+++ src/index.js
@@ -7,3 +7,16 @@ dotenv.config({
 })
 
 connectDB()
+
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Listening on PORT ${process.env.PORT}`);
+        
+    })
+})
+
+.catch((err)=>{
+    console.log('Db connection failed!!',err);
+    
+})
+
